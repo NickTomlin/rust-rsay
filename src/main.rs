c5108210a948d0856a58319ad16e5865a649603a
Nick Tomlin <nick.tomlin@gmail.com>
2016-06-28 12:16:42 +0100

    feat: start breaking things up to handle multiple lines

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::io::prelude::*;
 
+const GNU: &'static str = include_str!("gnu.txt");
+// todo: accept a wraplength
+const LINE_LENGTH: usize = 40;
+
 fn main () {
     let phrases: Vec<String> = std::env::args()
         .skip(1)
@@ -10,19 +14,43 @@ fn main () {
         std::process::exit(1);
     }
 
-    let gnu = include_str!("gnu.txt");
-    let phrase = phrases.join("");
+    let p = format_line(phrases);
 
-    // todo, format this to match the length of the longest string
-    println!(" _______");
-    println!("< {} >", phrase);
-    println!(" _______");
-    println!("{}",  gnu);
+    println!("{}", p);
+
+    // let phrase = phrases.join("");
+    // let mut combined = String::new();
+    // format_line(phrase, &mut &combined);
+
+    // // todo, format this to match the length of the longest string
+    // println!(" _______");
+    // print!("< {} >", combined);
+    // println!(" _______");
+    // println!("{}",  gnu);
+}
+
+fn multiline (phrase: String) -> String {
+    String::from("hey")
+}
+
+pub fn format_line (phrases: Vec<String>) -> String {
+    let phrase = phrases.join(" ");
+    let lines = phrase.chars().count() / LINE_LENGTH;
+    let border = (0..LINE_LENGTH).map(|_| "_").collect::<String>();
+
+    let formatted = match lines {
+        0 =>  String::from("< ") + &phrase + " >",
+        _ => multiline(phrase)
+    };
+
+    // we could "takewhile" the string has stuff in it, in increments of 40 characters
+    format!("{border}\n{formatted}\n{border}\n{gnu}", formatted = formatted, gnu = GNU, border = border)
 }
 
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+#[test]
+fn test_format_line () {
+    let mut x: &str = "";
+    format_line("Foo".to_string(), "Bar".to_string(), x);
+    assert_eq!("Bar Foo", x);
 }
